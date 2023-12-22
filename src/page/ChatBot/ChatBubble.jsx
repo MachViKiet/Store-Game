0340c3895b07649ac32c541cd4adfed5db07976e
MachKiet <MachViKiet>
2023-12-23 01:48:42 +0700

    update search

--- src/page/ChatBot/ChatBubble.jsx
+++ src/page/ChatBot/ChatBubble.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import Avatar from '@mui/material/Avatar';
 import ChatIcon from '@mui/icons-material/Chat';
 import Box from '@mui/material/Box';
-import ChatBox from './Chatbox';
+import ChatBox from '~/ChatBot/Chatbox';
 
 export const ChatBubble = () => {
   const [showChatBox, setShowChatBox] = useState(false);
